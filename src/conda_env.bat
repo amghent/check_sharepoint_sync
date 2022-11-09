6b2eb98e75c4ef6eb163d4a5806c3add3394e8d2
Yves Vindevogel <yves.vindevogel.external@arcelormittal.com>
2022-11-09 17:10:45 +0100

    Bugfix in pyyaml package

--- src/conda_env.bat
+++ src/conda_env.bat
@@ -1,4 +1,4 @@
 conda activate base
 conda create -y --name check_sharepoint_sync
 conda activate check_sharepoint_sync
-conda install -y -c conda-forge python=3.10 croniter git yaml
+conda install -y -c conda-forge python=3.10 croniter git pyyaml
